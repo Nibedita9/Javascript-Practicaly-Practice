373d189111c15d0b30a3f8a12800bf858d05cde2
Nibedita <nibeditarana7@gmail.com>
2024-03-15 20:29:57 +0530

    expression and operators don

--- Expressions/scirpt.js
+++ Expressions/scirpt.js
@@ -35,9 +35,49 @@ console.log(8 <= 8); // true
 console.log(9 >= 3); // true
 
 
+
+
 // logical && , || , ! 
 
 console.log(3 == 3 &&  4== 8); // false
 console.log(3 == 3 ||  4== 8); // true
 
+
+// write a programm that determines if a person is eligible 
+// to drive based on their age being greater than or equal to 18 and having a valid driver license 
+
+// dry right case
+// should be :- 
+// age=18 
+// age >= 18 
+// driverlicense=true then he or she can eligible for driving
+
+
+// let age=prompt("what is your age ?");
+let age=33;
+let drivingLicense=true;
+if(age >=18 && drivingLicense==true){
+    console.log(" Congratulations , you are eligible for drive");
+}
+else{
+    console.log("you can not drive");
+
+}
+
+
+// ternary operator 
+// condition ?true:false;
+
+
+let aged = 18;
+let result = aged>=56 ? "eligible" :"not eligible ";
+console.log(result);
+
+
+let score = 80;
+let resultMark = score>60 ? "pass" :"not pass ";
+console.log(resultMark);
+
+console.log(9 < 78 < 0); // true
+console.log("20" +10 +10); // true
 
