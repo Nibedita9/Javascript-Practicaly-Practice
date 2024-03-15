3dbcb0149de46db73f32b6bd7dfc29157c972723
Nibedita <nibeditarana7@gmail.com>
2024-03-15 17:12:08 +0530

    expression and operators practice

---
+++ Expressions/scirpt.js
@@ -0,0 +1,43 @@
+// Expression and operators
+
+// types of opeartors assignment ,arithmetic , comparisio , logical,string,unary,ternary , type 
+
+// console.log();
+
+
+// assignment =,/,+,*,-
+
+let a=9;
+let b=7;
+let sum=a+b;
+console.log(sum);
+
+let divide ="hello" / b;
+console.log(divide); // NAN  becaus we can not do this becasuse string and number
+
+let chec=0.1 + 0.1;
+console.log(chec);// 1.6
+
+
+// comparision == ,< , >
+
+console.log(3 == 3); // true
+console.log("3" == 3); // true
+
+console.log(3 === 3); // true
+console.log("3" === 3); // false because one string and another number data type  with value checked 
+
+console.log(8 < 8); // false
+console.log(9>  3); // true
+
+
+console.log(8 <= 8); // true
+console.log(9 >= 3); // true
+
+
+// logical && , || , ! 
+
+console.log(3 == 3 &&  4== 8); // false
+console.log(3 == 3 ||  4== 8); // true
+
+
